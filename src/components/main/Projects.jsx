16521a29876d8405a503089f3b824a37961997c8
CristianG07 <crisgonzalez07g@gmail.com>
2023-01-24 10:13:53 -0500

    update

--- src/components/main/Projects.jsx
+++ src/components/main/Projects.jsx
@@ -1,7 +1,6 @@
 import project1 from "../../assets/images/project-1.jpg";
 import project2 from "../../assets/images/project-2.jpg";
 import project3 from "../../assets/images/project-3.jpg";
-import project4 from "../../assets/images/project-4.jpg";
 import CardsPeject from "./CardsProject";
 
 const Projects = () => {
